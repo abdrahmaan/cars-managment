f173b0239058b76829ec37934b871d98133b5c89
Abdelrahman Hamdy <xmbedo@gmail.com>
2024-01-31 11:01:08 +0200

    Edit Footer Data

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,10 +3,10 @@
     
 
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2023-2024 <a href="https://wa.me/201110645479" target="_blank">Abdulrahman Hamdy</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0
+      <b>Version</b> 1.0.0
     </div>
 </footer>
 
